85770c4a4f80215f1fe981c6eb72da3dea7b2ff4
daniel <dpineda50@gmail.com>
2017-06-12 22:25:49 -0700

    simple and adaptive thresholding

--- simple_thresholding.py
+++ simple_thresholding.py
@@ -8,13 +8,18 @@ ap.add_argument("-i","--image",required = True,
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(image, (5,5), 0)
-cv2.imshow("Image", image)
-(T, thresh) = cv2.threshold(blurred, 155, 255, cv2.THRESH_BINARY)
-cv2.imshow("Threshold Binary", thresh)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.medianBlur(gray,3)
+#cv2.imshow("Image", image)
 
-(T, threshInv) = cv2.threshold(blurred, 155, 255, cv2.THRESH_BINARY_INV)
-cv2.imshow("Threshold Binary Inverse", threshInv)
-cv2.imshow("Coins", cv2.bitwise_and(image, image, mask = threshInv))
-cv2.waitKey(0)
+(T, thresh) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
+#cv2.imshow("Threshold Binary", thresh)
+
+(T, threshInv) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY_INV)
+#cv2.imshow("Threshold Binary Inverse", threshInv)
+
+#cv2.imshow("Coins", cv2.bitwise_and(image, image, mask = threshInv))
+
+#cv2.waitKey(0)
+
+
