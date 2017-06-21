0089629175bbb7532f4a03b6469038591dd759da
daniel <dpineda50@gmail.com>
2017-06-20 20:02:24 -0700

    Edge Detection / Canny / Auto-Canny

---
+++ auto_canny.py
@@ -0,0 +1,23 @@
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the image")
+args = vars(ap.parse_args())
+
+#load image
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (3,3), 0)
+
+#apply Canny edge detection using wide, tight and auto threshold
+wide = cv2.Canny(blurred, 10, 200)
+tight = cv2.Canny(blurred, 225, 250)
+auto = imutils.auto_canny(blurred)
+
+cv2.imshow("Original", image)
+cv2.imshow("Wide", wide)
+cv2.imshow("Tight", tight)
+cv2.imshow("Auto", auto)
+cv2.waitKey(0)
--- imutils.py
+++ imutils.py
@@ -34,3 +34,13 @@ def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 
     resized = cv2.resize(image, dim, interpolation = inter)
     return resized
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensity
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0+sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    return edged
